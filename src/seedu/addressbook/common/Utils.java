d9c608295c076d86808f9e0c10dde5f9f1461195
nianfei97 <tehnianfei@gmail.com>
2018-09-07 23:22:32 +0800

    reduced import scope of FindCommand.java and Utils.java

--- src/seedu/addressbook/common/Utils.java
+++ src/seedu/addressbook/common/Utils.java
@@ -1,6 +1,10 @@
 package seedu.addressbook.common;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Utility methods
