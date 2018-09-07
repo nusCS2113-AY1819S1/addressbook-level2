5f93ea1a215a08f9a01112b59c6ede18e4e2bd2f
Arjo Chakravarty <arjo129@gmail.com>
2018-09-07 15:14:21 +0800

    Updated help text

--- src/seedu/addressbook/commands/EditNameCommand.java
+++ src/seedu/addressbook/commands/EditNameCommand.java
@@ -6,11 +6,10 @@ import seedu.addressbook.data.person.*;
 
 public class EditNameCommand extends Command {
     public static final String COMMAND_WORD = "edit_name";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Modifies a persons name from the addressbook "
-            + "Contact details can be marked private by prepending 'p' to the prefix.\n"
-            + "Parameters: NAME [p]p/PHONE [p]e/EMAIL [p]a/ADDRESS  [t/TAG]...\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Updates the name of a user.\n"
+            + "Parameters: Old Name n/New Name\n"
             + "Example: " + COMMAND_WORD
-            + " John Doe p/98765432 e/johnd@gmail.com a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney";
+            + " John Doe n/New Name";
     private static final String MESSAGE_DUPLICATE = "Cannot update address book to same name";
     public static final String MESSAGE_SUCCESS = "Person updated: %1$s";
     private String oldPersonName, newPersonName;
