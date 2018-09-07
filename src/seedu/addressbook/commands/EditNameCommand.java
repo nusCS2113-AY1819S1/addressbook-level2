f160ba47d8d29be885f364f5b7d12b78af887750
Arjo Chakravarty <arjo129@gmail.com>
2018-09-07 15:08:39 +0800

    Implemented method

--- src/seedu/addressbook/commands/EditNameCommand.java
+++ src/seedu/addressbook/commands/EditNameCommand.java
@@ -1,5 +1,9 @@
 package seedu.addressbook.commands;
 
+import seedu.addressbook.common.Messages;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.*;
+
 public class EditNameCommand extends Command {
     public static final String COMMAND_WORD = "edit_name";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Modifies a persons name from the addressbook "
@@ -7,14 +11,42 @@ public class EditNameCommand extends Command {
             + "Parameters: NAME [p]p/PHONE [p]e/EMAIL [p]a/ADDRESS  [t/TAG]...\n"
             + "Example: " + COMMAND_WORD
             + " John Doe p/98765432 e/johnd@gmail.com a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney";
-
+    private static final String MESSAGE_DUPLICATE = "Cannot update address book to same name";
+    public static final String MESSAGE_SUCCESS = "Person updated: %1$s";
     private String oldPersonName, newPersonName;
-    public EditNameCommand(String name, String password){
+    public EditNameCommand(String name, String newName){
         this.oldPersonName = name;
-        this.newPersonName = name;
+        this.newPersonName = newName;
     }
     @Override
     public CommandResult execute(){
-        return new CommandResult("hi");
+        UniquePersonList people = addressBook.getAllPersons();
+        ReadOnlyPerson toBeRemoved = null;
+        for(ReadOnlyPerson person: people.immutableListView()){
+            if(person.getName().fullName.equals(this.oldPersonName)){
+                toBeRemoved = person;
+            }
+        }
+        if(toBeRemoved == null){
+            return  new CommandResult(Messages.MESSAGE_PERSON_NOT_IN_ADDRESSBOOK+this.oldPersonName);
+        }
+        try {
+            Person newEntry = new Person(
+                    new Name(this.newPersonName),
+                    toBeRemoved.getPhone(),
+                    toBeRemoved.getEmail(),
+                    toBeRemoved.getAddress(),
+                    toBeRemoved.getTags()
+            );
+            addressBook.addPerson(newEntry);
+            addressBook.removePerson(toBeRemoved);
+            return new CommandResult(String.format(MESSAGE_SUCCESS, newEntry));
+        } catch (UniquePersonList.DuplicatePersonException dpe){
+            return new CommandResult(MESSAGE_DUPLICATE);
+        } catch (UniquePersonList.PersonNotFoundException cpe){
+            return new CommandResult(Messages.MESSAGE_PERSON_NOT_IN_ADDRESSBOOK+this.oldPersonName);
+        } catch (IllegalValueException ive){
+            return new CommandResult(ive.getMessage());
+        }
     }
 }
