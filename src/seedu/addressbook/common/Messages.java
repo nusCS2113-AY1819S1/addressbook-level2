bb02dfe1964930f1b1877ff4f4b303fa8709e0d0
DESKTOP-3LC2JA7\Novin <e0176909@u.nus.edu>
2018-09-08 00:01:34 +0800

    added edit command to user guide

--- src/seedu/addressbook/common/Messages.java
+++ src/seedu/addressbook/common/Messages.java
@@ -15,5 +15,4 @@ public class Messages {
             "java seedu.addressbook.Main [STORAGE_FILE_PATH]";
     public static final String MESSAGE_WELCOME = "Welcome to your Address Book!";
     public static final String MESSAGE_USING_STORAGE_FILE = "Using storage file : %1$s";
-    public static final String MESSAGE_INVALID_EDIT_PARAM = "Edit parameters invalid, please try again";
 }
