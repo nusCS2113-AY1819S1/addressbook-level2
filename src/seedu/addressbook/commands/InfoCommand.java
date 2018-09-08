27148ee9c7bf6d3d08fdd818854cd61a8a074be4
Zeng Hao <zenghaonus@outlook.com>
2018-09-08 16:00:24 +0800

    entering info command can print out the correct information

--- src/seedu/addressbook/commands/InfoCommand.java
+++ src/seedu/addressbook/commands/InfoCommand.java
@@ -1,9 +1,15 @@
 package seedu.addressbook.commands;
 
 public class InfoCommand extends Command{
+    public static final String COMMAND_WORD= "info";
+    public static final String MESSAGE_USAGE= COMMAND_WORD+": print out several information"+"\n"+"Example: "+COMMAND_WORD ;
+    public static final String INFO ="This is the addressbook level2 version 0.1 "
+    + "\n"+ "Developed by Zeng Hao(Oscar)";
 
-
-
-
+    public InfoCommand () {}
+    @Override
+    public CommandResult execute() {
+        return new CommandResult(INFO);
+    }
 
 }
