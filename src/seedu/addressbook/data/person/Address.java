88365345068ab96202ac6f28b6a613ff9220718e
Brandon <brandonccm@hotmail.com>
2018-09-09 00:34:34 +0800

    Extracted common methods into parent Contact class
    Classified all methods in the Address, Contact and Email classes together and moved them into parent Contact class

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -12,43 +12,12 @@ public class Address extends Contact{
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
 
-    public final String value;
-
     /**
      * Validates given address.
      *
      * @throws IllegalValueException if given address string is invalid.
      */
     public Address(String address, boolean isPrivate) throws IllegalValueException {
-        super(isPrivate);
-        String trimmedAddress = address.trim();
-        if (!isValidAddress(trimmedAddress)) {
-            throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
-        }
-        this.value = trimmedAddress;
-    }
-
-    /**
-     * Returns true if a given string is a valid person address.
-     */
-    public static boolean isValidAddress(String test) {
-        return test.matches(ADDRESS_VALIDATION_REGEX);
-    }
-
-    @Override
-    public String toString() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof Address // instanceof handles nulls
-                && this.value.equals(((Address) other).value)); // state check
-    }
-
-    @Override
-    public int hashCode() {
-        return value.hashCode();
+        super(address, isPrivate, ADDRESS_VALIDATION_REGEX, MESSAGE_ADDRESS_CONSTRAINTS);
     }
 }
--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -1,11 +1,56 @@
 package seedu.addressbook.data.person;
 
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Represents a Person's contact(address/phone/email) in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ */
 public class Contact {
 
+    public final String value;
     private boolean isPrivate;
+    public static String regex;
+    public String constraints;
 
-    public Contact(boolean isPrivate){
+    /**
+     * Validates given contact.
+     *
+     * @throws IllegalValueException if given address string is invalid.
+     */
+    public Contact(String contact, boolean isPrivate, String regex, String constraints) throws IllegalValueException{
         this.isPrivate = isPrivate;
+        this.regex = regex;
+        this.constraints = constraints;
+        String trimmedContact = contact.trim();
+        if (!isValidContact(trimmedContact)) {
+            throw new IllegalValueException(constraints);
+        }
+        this.value = trimmedContact;
+    }
+
+    /**
+     * Returns true if a given string is a valid person contact (address/phone/email).
+     */
+    public static boolean isValidContact(String test) {
+        return test.matches(regex);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Contact // instanceof handles nulls
+                && this.value.equals(((Contact) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
     }
 
     public boolean isPrivate() {
