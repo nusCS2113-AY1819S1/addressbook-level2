94c311154ab86272aa58d078032d1fc3c94ef222
Vincent <vencintgamer@gmail.com>
2018-09-08 19:20:56 +0800

    Moved the common attributes into super class "Contact"

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Represents a standard contact parameter
+ */
+public class Contact {
+
+    public String value;
+    protected boolean isPrivate;
+
+}
