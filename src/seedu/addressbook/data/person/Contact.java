ac34cd0774a0668f5754467755f093629bfb111d
Tan Wei Ming <e0176337@u.nus.edu>
2018-09-09 11:36:01 +0800

    Extracted Contact class from Phone, Email and Address classes

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,30 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Represents an person's contact.
+ */
+public class Contact {
+    public String value;
+    protected boolean isPrivate;
+
+    protected Contact(){
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Contact // instanceof handles nulls
+                && this.value.equals(((Contact) other).value)); // state check
+    }
+
+    public String toString() {
+        return value;
+    }
+
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
