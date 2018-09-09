eb95abec46774d81db7c6e39a923ee7d22708dfa
DESKTOP-3LC2JA7\Novin <e0176909@u.nus.edu>
2018-09-09 21:00:45 +0800

    add invalid index testing to EditCommandTest.java

--- test/java/seedu/addressbook/commands/EditCommandTest.java
+++ test/java/seedu/addressbook/commands/EditCommandTest.java
@@ -2,21 +2,42 @@ package seedu.addressbook.commands;
 
 import org.junit.Test;
 
+import seedu.addressbook.common.Messages;
 import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 import seedu.addressbook.util.TypicalPersons;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class EditCommandTest {
-    private final AddressBook addressBook = new TypicalPersons().getTypicalAddressBook();
-    private final TypicalPersons td = new TypicalPersons();
+    private AddressBook addressBook = new TypicalPersons().getTypicalAddressBook();
+    private TypicalPersons td = new TypicalPersons();
+    private List<ReadOnlyPerson> listWithAllTypicalPersons = Arrays.asList(td.getTypicalPersons());
 
+    /**
+     * Test for invalid indexing of the edit command
+     */
     @Test
-    public void execute() {
-        //same word, same case: matched
+    public void execute_invalidIndex_returnsInvalidIndexMessage() {
+        assertEditFailsDueToInvalidIndex(0, addressBook, listWithAllTypicalPersons);
+        assertEditFailsDueToInvalidIndex(-1, addressBook, listWithAllTypicalPersons);
+        assertEditFailsDueToInvalidIndex(listWithAllTypicalPersons.size() + 1
+                                            ,addressBook, listWithAllTypicalPersons);
+    }
+
+    /**
+     * Asserts that the index is not valid for the given display list.
+     */
+    private void assertEditFailsDueToInvalidIndex(int invalidVisibleIndex, AddressBook addressBook,
+                                                      List<ReadOnlyPerson> displayList) {
+
+        String expectedMessage = Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
+
+        EditCommand command = createEditCommand(invalidVisibleIndex, addressBook, displayList,"p","99999999" );
+        assertCommandBehaviour(command, expectedMessage, addressBook, addressBook);
     }
 
     /**
@@ -37,14 +58,14 @@ public class EditCommandTest {
     /**
      * Executes the command, and checks that the execution was what we had expected.
      */
-    /*private void assertEditCommandBehaviour(DeleteCommand deleteCommand, String expectedMessage,
+    private void assertCommandBehaviour(EditCommand editCommand, String expectedMessage,
                                         AddressBook expectedAddressBook, AddressBook actualAddressBook) {
 
-        CommandResult result = deleteCommand.execute();
+        CommandResult result = editCommand.execute();
 
         assertEquals(expectedMessage, result.feedbackToUser);
         assertEquals(expectedAddressBook.getAllPersons(), actualAddressBook.getAllPersons());
-    }*/
+    }
 
 
 
