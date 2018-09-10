a680c7e0d65f7c0c05ba858af3219c72aa592de5
luhan02 <e0191702@u.nus.edu>
2018-09-11 00:14:19 +0800

    Case_insensitive

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,30 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Sorts all persons in the address book to the user.
+ */
+public class SortCommand {
+//    public static final String COMMAND_WORD = "list";
+//
+//    public static final String MESSAGE_USAGE = COMMAND_WORD
+//            + ": Displays all persons in the address book as a list with index numbers, in alphabetical order.\n";
+//
+//
+//    @Override
+//    public CommandResult execute() {
+//        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+//        Collections.sort(allPersons, new Comparator<List[]>() {
+//            public int compare(String[] strings, String[] otherStrings) {
+//                return strings[0].compareTo(otherStrings[0]);
+//            }
+//        });
+//
+//        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+//    }
+}
