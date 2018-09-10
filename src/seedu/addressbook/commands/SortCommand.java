7d959303afec3b7dc83b2d62857baf4de1016a3d
sxqnicholas96 <37098263+sxqnicholas96@users.noreply.github.com>
2018-09-10 17:07:31 +0800

    Updated user guide

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -26,5 +26,5 @@ public class SortCommand extends Command {
         allPersons.sort(customComparator);
         
         return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
-    }
+    }   
 }
