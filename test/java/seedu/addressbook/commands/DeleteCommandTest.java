ec4db02753c060acd38d51a265460d5d93fe344e
Kha Sim <ks.beh96@gmail.com>
2018-09-11 13:24:08 +0800

    Resolved conflicts in test files

--- test/java/seedu/addressbook/commands/DeleteCommandTest.java
+++ test/java/seedu/addressbook/commands/DeleteCommandTest.java
@@ -1,25 +1,19 @@
 package seedu.addressbook.commands;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import seedu.addressbook.common.Messages;
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.*;
+import seedu.addressbook.data.person.UniquePersonList.PersonNotFoundException;
+import seedu.addressbook.ui.TextUi;
+import seedu.addressbook.util.TestUtil;
 
 import java.util.Collections;
 import java.util.List;
 
-import org.junit.Before;
-import org.junit.Test;
-
-import seedu.addressbook.common.Messages;
-import seedu.addressbook.data.AddressBook;
-import seedu.addressbook.data.exception.IllegalValueException;
-import seedu.addressbook.data.person.Address;
-import seedu.addressbook.data.person.Email;
-import seedu.addressbook.data.person.Name;
-import seedu.addressbook.data.person.Person;
-import seedu.addressbook.data.person.Phone;
-import seedu.addressbook.data.person.ReadOnlyPerson;
-import seedu.addressbook.data.person.UniquePersonList.PersonNotFoundException;
-import seedu.addressbook.ui.TextUi;
-import seedu.addressbook.util.TestUtil;
+import static org.junit.Assert.assertEquals;
 
 public class DeleteCommandTest {
 
