119927019e3cd76101271cfb7ea63a74bc309f29
luhan02 <e0191702@u.nus.edu>
2018-09-11 20:08:09 +0800

    ignoreCase & SortCommand

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -5,26 +5,22 @@ import seedu.addressbook.data.person.ReadOnlyPerson;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.SortedSet;
 
 /**
  * Sorts all persons in the address book to the user.
  */
-public class SortCommand {
-//    public static final String COMMAND_WORD = "list";
-//
-//    public static final String MESSAGE_USAGE = COMMAND_WORD
-//            + ": Displays all persons in the address book as a list with index numbers, in alphabetical order.\n";
-//
-//
-//    @Override
-//    public CommandResult execute() {
-//        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
-//        Collections.sort(allPersons, new Comparator<List[]>() {
-//            public int compare(String[] strings, String[] otherStrings) {
-//                return strings[0].compareTo(otherStrings[0]);
-//            }
-//        });
-//
-//        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
-//    }
+public class SortCommand extends Command{
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Displays all persons in the address book as a list with index numbers, in alphabetical order.\n"
+            + "Example: " + COMMAND_WORD;;
+
+
+    @Override
+    public CommandResult execute() {
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().sortedList();
+        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+    }
 }
