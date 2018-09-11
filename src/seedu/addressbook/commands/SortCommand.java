8110ddce9e740bee748ad9a32b05c20166590882
Xian Hao <xianhao@u.nus.edu>
2018-09-11 12:49:56 +0800

    -Added SortCommand class -Added ability to accept "sort" command in Parser

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,22 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import java.util.List;
+
+/**
+ * Sort all persons in the lexicographical order.
+ */
+public class SortCommand extends Command {
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Sort all persons in the address book in lexicographical order.\n"
+            + "Example: " + COMMAND_WORD;
+
+
+    @Override
+    public CommandResult execute() {
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+    }
+}
