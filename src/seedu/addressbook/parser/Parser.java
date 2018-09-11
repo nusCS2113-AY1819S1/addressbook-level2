12f86af2206e7401e8605a8230f6c1ec6d980983
jamesyaputra <jamesyaputra1704@gmail.com>
2018-09-11 09:49:22 +0800

    Added commands/EditCommand class & edit Parrser.java

--- src/seedu/addressbook/parser/Parser.java
+++ src/seedu/addressbook/parser/Parser.java
@@ -14,6 +14,7 @@ import java.util.regex.Pattern;
 import seedu.addressbook.commands.AddCommand;
 import seedu.addressbook.commands.ClearCommand;
 import seedu.addressbook.commands.Command;
+import seedu.addressbook.commands.EditCommand;
 import seedu.addressbook.commands.DeleteCommand;
 import seedu.addressbook.commands.ExitCommand;
 import seedu.addressbook.commands.FindCommand;
@@ -77,6 +78,9 @@ public class Parser {
 
         case AddCommand.COMMAND_WORD:
             return prepareAdd(arguments);
+
+            case EditCommand.COMMAND_WORD:
+                return prepareDelete(arguments);
 
         case DeleteCommand.COMMAND_WORD:
             return prepareDelete(arguments);
