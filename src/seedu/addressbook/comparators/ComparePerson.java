238f2c4eafbdb1844ae25c137e065f325556eebd
jitwei98 <limjitwei@gmail.com>
2018-09-11 13:51:07 +0800

    Add comments for the ComparePerson class

--- src/seedu/addressbook/comparators/ComparePerson.java
+++ src/seedu/addressbook/comparators/ComparePerson.java
@@ -4,6 +4,9 @@ import java.util.Comparator;
 
 import seedu.addressbook.data.person.Person;
 
+/**
+ * Compare the lexicographical ordering of the name of two persons
+ */
 public class ComparePerson implements Comparator<Person> {
 
     @Override
