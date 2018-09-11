281e64b101876b39646784c5a883e24d8a75d2b0
auskure <oscarlwj@hotmail.com>
2018-09-11 13:19:26 +0800

    Added a new test case, and correctly modified the expected test results.

---
+++ src/seedu/addressbook/ui/Formatter.java
@@ -0,0 +1,15 @@
+package seedu.addressbook.ui;
+
+public class Formatter {
+
+    /** A decorative prefix added to the beginning of lines printed by AddressBook */
+    public static final String LINE_PREFIX = "|| ";
+
+    /** A platform independent line separator. */
+    public static final String LS = System.lineSeparator();
+
+    public static final String DIVIDER = "===================================================";
+
+    /** Format of indexed list item */
+    public static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
+}
