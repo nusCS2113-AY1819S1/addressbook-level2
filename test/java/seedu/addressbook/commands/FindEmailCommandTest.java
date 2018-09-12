a2d7d45fef1480165b004f51989d93d11e28e14f
trufflepirate <rootfourtytwo@gmail.com>
2018-09-12 21:48:57 +0800

    Changed JUNIT tests. works now. Re-did the gradle thingy.

--- test/java/seedu/addressbook/commands/FindEmailCommandTest.java
+++ test/java/seedu/addressbook/commands/FindEmailCommandTest.java
@@ -18,39 +18,39 @@ public class FindEmailCommandTest {
     @Test
     public void execute() throws IllegalValueException {
         //same word, same case: matched
-        assertFindCommandBehavior(new String[]{"ab@gmail.com"}, Arrays.asList(td.amy));
+        assertFindEmailCommandBehavior(new String[]{"ab@gmail.com"}, Arrays.asList(td.amy));
 
         //same word, different case: not matched
-        assertFindCommandBehavior(new String[]{"aB@gmail.com"}, Collections.emptyList());
+        assertFindEmailCommandBehavior(new String[]{"aB@gmail.com"}, Collections.emptyList());
 
         //partial word: not matched
-        assertFindCommandBehavior(new String[]{"ab@gmail.co"}, Collections.emptyList());
+        assertFindEmailCommandBehavior(new String[]{"ab@gmail.co"}, Collections.emptyList());
 
         //multiple words: matched
-        assertFindCommandBehavior(new String[]{"ab@gmail.com", "bc@gmail.com", "cd@gmail.com"},
+        assertFindEmailCommandBehavior(new String[]{"ab@gmail.com", "bc@gmail.com", "cd@gmail.com"},
                 Arrays.asList(td.amy, td.bill, td.candy));
 
         //repeated keywords: matched
-        assertFindCommandBehavior(new String[]{"ab@gmail.com", "ab@gmail.com"}, Arrays.asList(td.amy));
+        assertFindEmailCommandBehavior(new String[]{"ab@gmail.com", "ab@gmail.com"}, Arrays.asList(td.amy));
 
         //Keyword matching a word in address: not matched
-        assertFindCommandBehavior(new String[]{"Clementi"}, Collections.emptyList());
+        assertFindEmailCommandBehavior(new String[]{"Clementi"}, Collections.emptyList());
     }
 
     /**
      * Executes the find command for the given keywords and verifies
      * the result matches the persons in the expectedPersonList exactly.
      */
-    private void assertFindCommandBehavior(String[] keywords, List<ReadOnlyPerson> expectedPersonList) {
-        FindCommand command = createFindCommand(keywords);
+    private void assertFindEmailCommandBehavior(String[] keywords, List<ReadOnlyPerson> expectedPersonList) {
+        FindEmailCommand command = createFindEmailCommand(keywords);
         CommandResult result = command.execute();
 
         assertEquals(Command.getMessageForPersonListShownSummary(expectedPersonList), result.feedbackToUser);
     }
 
-    private FindCommand createFindCommand(String[] keywords) {
+    private FindEmailCommand createFindEmailCommand(String[] keywords) {
         final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
-        FindCommand command = new FindCommand(keywordSet);
+        FindEmailCommand command = new FindEmailCommand(keywordSet);
         command.setData(addressBook, Collections.emptyList());
         return command;
     }
