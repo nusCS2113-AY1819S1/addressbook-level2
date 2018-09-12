c33234435111fc71dcae1a2cbe9f29e226a161c9
Daryl Nigel Lim En Wei <e0031924@u.nus.edu>
2018-09-12 22:53:21 +0800

    rename 'clear' to 'clearAddressBook' for clarity

--- src/seedu/addressbook/commands/ClearCommand.java
+++ src/seedu/addressbook/commands/ClearCommand.java
@@ -3,15 +3,15 @@ package seedu.addressbook.commands;
 /**
  * Clears the address book.
  */
-public class ClearCommand extends Command {
+public class ClearAddressBookCommand extends Command {
 
-    public static final String COMMAND_WORD = "clear";
+    public static final String COMMAND_WORD = "clearAddressBook";
     public static final String MESSAGE_USAGE = "Clears address book permanently.\n"
             + "Example: " + COMMAND_WORD;
 
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
 
-    public ClearCommand() {}
+    public ClearAddressBookCommand() {}
 
 
     @Override
