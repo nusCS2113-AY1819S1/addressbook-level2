c527aed63cf28c598b8816dd3cd9e9544ba08f2c
sarahnifsa <sarahtb25@gmail.com>
2018-09-12 22:35:46 +0800

    Revert "Updated test cases, so code passes all test cases."
    This reverts commit c66ee3631d0e1c8923c86db69edcaba59f912e10.

--- test/java/seedu/addressbook/commands/AddCommandTest.java
+++ test/java/seedu/addressbook/commands/AddCommandTest.java
@@ -125,7 +125,7 @@ public class AddCommandTest {
 
         assertTrue(people.contains(p));
         assertEquals(1, people.immutableListView().size());
-        assertTrue(result.getRelevantPersons().isPresent());
+        assertFalse(result.getRelevantPersons().isPresent());
         assertEquals(String.format(AddCommand.MESSAGE_SUCCESS, p), result.feedbackToUser);
     }
 
