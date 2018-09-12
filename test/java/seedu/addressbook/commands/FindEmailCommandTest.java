53a399d9ec520258dd29f9f601bd15560d81b4de
trufflepirate <rootfourtytwo@gmail.com>
2018-09-12 21:21:19 +0800

    +added some tests

---
+++ test/java/seedu/addressbook/commands/FindEmailCommandTest.java
@@ -0,0 +1,58 @@
+package seedu.addressbook.commands;
+
+import org.junit.Test;
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.util.TypicalPersons;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class FindEmailCommandTest {
+
+    private final AddressBook addressBook = new TypicalPersons().getTypicalAddressBook();
+    private final TypicalPersons td = new TypicalPersons();
+
+    @Test
+    public void execute() throws IllegalValueException {
+        //same word, same case: matched
+        assertFindCommandBehavior(new String[]{"ab@gmail.com"}, Arrays.asList(td.amy));
+
+        //same word, different case: not matched
+        assertFindCommandBehavior(new String[]{"aB@gmail.com"}, Collections.emptyList());
+
+        //partial word: not matched
+        assertFindCommandBehavior(new String[]{"ab@gmail.co"}, Collections.emptyList());
+
+        //multiple words: matched
+        assertFindCommandBehavior(new String[]{"ab@gmail.com", "bc@gmail.com", "cd@gmail.com"},
+                Arrays.asList(td.amy, td.bill, td.candy));
+
+        //repeated keywords: matched
+        assertFindCommandBehavior(new String[]{"ab@gmail.com", "ab@gmail.com"}, Arrays.asList(td.amy));
+
+        //Keyword matching a word in address: not matched
+        assertFindCommandBehavior(new String[]{"Clementi"}, Collections.emptyList());
+    }
+
+    /**
+     * Executes the find command for the given keywords and verifies
+     * the result matches the persons in the expectedPersonList exactly.
+     */
+    private void assertFindCommandBehavior(String[] keywords, List<ReadOnlyPerson> expectedPersonList) {
+        FindCommand command = createFindCommand(keywords);
+        CommandResult result = command.execute();
+
+        assertEquals(Command.getMessageForPersonListShownSummary(expectedPersonList), result.feedbackToUser);
+    }
+
+    private FindCommand createFindCommand(String[] keywords) {
+        final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
+        FindCommand command = new FindCommand(keywordSet);
+        command.setData(addressBook, Collections.emptyList());
+        return command;
+    }
+
+}
