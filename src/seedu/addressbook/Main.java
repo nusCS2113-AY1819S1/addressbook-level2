ca1dfa7d2191d9861730f70d30722b78ec47f0f3
wm28 <e0176337@u.nus.edu>
2018-09-24 14:03:40 +0800

    Make StorageOperationException to not terminate program if it occurs

--- src/seedu/addressbook/Main.java
+++ src/seedu/addressbook/Main.java
@@ -111,7 +111,9 @@ public class Main {
             CommandResult result = command.execute();
             storage.save(addressBook);
             return result;
-        } catch (Exception e) {
+        } catch (StorageOperationException soe) {
+            return new CommandResult(soe.getMessage());
+        }catch (Exception e) {
             ui.showToUser(e.getMessage());
             throw new RuntimeException(e);
         }
